26dba54dd16316017e6e2af80663e21db197fc6d
hdemby <hdemby@unc.edu>
2022-06-05 20:01:02 -0400

    Using templates for GET and POST information

--- webapp.py
+++ webapp.py
@@ -1,18 +1,26 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
+"""@app.route("/")
+def fauxhome():
     return "Hello! Hi there! This is <h1>Flask</h1>"
 
 @app.route("/<name>")
-def user(name):
-    return f"Hello {name}!"
+def home(name):
+    return render_template("index.html", content=name)
+"""
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        user = request.form["nm"]
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("login.html")
 
-@app.route("/admin")
-def admin():
-    return redirect(url_for("user", name = "Admin!"))
+@app.route("/<usr>")
+def user(usr):
+    return f"Hi <b>{usr}</b>! You have logged in!"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
